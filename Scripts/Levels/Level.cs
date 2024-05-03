f17465619d26f0406d4416793a5800fcbcc354bb
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-05-03 20:24:38 +0300

    Added a deathzone so when player falls , their position and speed are reset

--- Scripts/Levels/Level.cs
+++ Scripts/Levels/Level.cs
@@ -5,6 +5,21 @@ using System;
 namespace MartianMikeMono.Scripts.Levels;
 public partial class Level : Node2D
 {
+    [ExportGroup("Required")]
+    [Export]
+    public Area2D DeathZone { get; private set; }
+
+    [Export]
+    public Marker2D PlayerSpawnPosition { get; private set; }
+
+
+
+    // GameLoop Methods----------------------------------------------------------------------------
+    public override void _Ready()
+    {
+        DeathZone.BodyEntered += OnDeathZoneBodyEntered;
+    }
+
     public override void _Input(InputEvent @event)
     {
         if (Input.IsActionJustReleased(InputActionConstants.QUIT))
@@ -15,6 +30,21 @@ public partial class Level : Node2D
         if (Input.IsActionJustReleased(InputActionConstants.RESET))
         {
             GetTree().ReloadCurrentScene();
+        }
+    }
+
+
+    // Signal Methods------------------------------------------------------------------------------
+    /// <summary>
+    /// Reset the player speed to zero and its position to the spawn position
+    /// </summary>
+    /// <param name="body"></param>
+    private void OnDeathZoneBodyEntered(Node2D body)
+    {
+        if (body is Player player)
+        {
+            player.Velocity = Vector2.Zero;
+            player.GlobalPosition = PlayerSpawnPosition.Position;
         }
     }
 }
