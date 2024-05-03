6d7208cd713dfbd60088988bbda642ddbc53a290
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-05-03 17:32:12 +0300

    Added a the movement logic for the player(move left/right, jump, fall) and added a temporary ground for testing in the level scene

--- Scripts/Characters/Player.cs
+++ Scripts/Characters/Player.cs
@@ -1,4 +1,5 @@
 using Godot;
+using MartianMikeMono.Scripts.Helper;
 using System;
 
 public partial class Player : CharacterBody2D
@@ -13,16 +14,35 @@ public partial class Player : CharacterBody2D
     [Export]
     private float _gravity = 400.0f;
 
+    [Export]
+    private float _jumpForce = 200.0f;
+
+    [Export]
+    private float _speed = 125.0f;
+
+    
+    // Hold the direction of the player movement to apply it to the velocity
     private Vector2 _movementVector;
 
 
 
     public override void _PhysicsProcess(double delta)
     {
+        // Apply gravity
         if (!IsOnFloor())
         {
             _movementVector.Y += _gravity * (float)delta;
         }
+
+        // Apply jump
+        if (Input.IsActionJustPressed(InputActionConstants.JUMP))
+        {
+            _movementVector.Y = -1 * _jumpForce;
+        }
+
+        // Apply left and right movement
+        float direction = Input.GetAxis(InputActionConstants.MOVE_LEFT, InputActionConstants.MOVE_RIGHT);
+        _movementVector.X = direction * _speed;
 
         Velocity = _movementVector;
         
